8e875dce285b9c96a0f7b552305ad463281dd4e8
arnaldqueiroga <arnaldqueiroga@gmail.com>
2021-03-12 12:38:03 -0300

    04 Checked e Unchecked - Hierarquia de exceções

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -12,7 +12,7 @@ public class Fluxo {
         try {
         	metodo2();
            	
-        } catch (ArithmeticException | NullPointerException ex) {
+        } catch (ArithmeticException | NullPointerException | MinhaExcecao ex) {
         	String msg = ex.getMessage();
         	System.out.println("Exception " + msg);
         	ex.printStackTrace();
@@ -24,15 +24,9 @@ public class Fluxo {
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-            // int a = i / 0;  
-            Conta c = null;
-         //   c.deposita();
-            
-            
-        }
-        System.out.println("Fim do metodo2");
+        throw new MinhaExcecao("Deu muito errado!");
+     
+        //System.out.println("Fim do metodo2");
     }
 
 }
